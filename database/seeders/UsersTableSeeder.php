877957446a5125c549b8ef7aaa082cda74ce3135
Nancy <nancy.gupta@mail.vinove.com>
2022-06-14 16:25:33 +0530

    first commit

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -13,15 +13,15 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-     $faker = Faker\Factory::create();
-     foreach (range(1,50) as $index) {
-      $data = [
-       'name' => $faker->name,
-       'email' => $faker->unique()->safeEmail,
-       'password' => bcrypt('secret'),
-       'remember_token' => Str::random(10),
-      ];
-      User::create($data);
-     }
+        $faker = Faker\Factory::create();
+        foreach (range(1, 50) as $index) {
+            $data = [
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'password' => bcrypt('secret'),
+                'remember_token' => Str::random(10),
+            ];
+            User::create($data);
+        }
     }
 }
