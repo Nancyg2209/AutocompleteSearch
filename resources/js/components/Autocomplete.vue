cc3737bb6704585ac96f6b5dbd0efc32bc679d08
Nancy <nancy.gupta@mail.vinove.com>
2022-06-14 14:29:59 +0530

    first commit

---
+++ resources/js/components/Autocomplete.vue
@@ -0,0 +1,25 @@
+<template>
+ <div>
+  <input type="text" placeholder="Search?" v-model="query" v-on:keyup="autoComplete" class="form-control">
+  </div>
+</template>
+<script>
+ export default{
+  data(){
+   return {
+    query: '',
+    results: []
+   }
+  },
+  methods: {
+   autoComplete(){
+    this.results = [];
+    if(this.query.length > 2){
+     axios.get('/api/search',{params: {query: this.query}}).then(response => {
+      this.results = response.data;
+     });
+    }
+   }
+  }
+ }
+</script>
